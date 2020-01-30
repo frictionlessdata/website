9d28278db85037671685fdc656167b4ba078e98d
Rufus Pollock <rufus.pollock@datopian.com>
2020-01-30 12:52:30 +0100

    [config][s]: set up CNAME for next.frictionlessdata.io.
    * also remove base name config as now we can be at root

--- site/.vuepress/config.js
+++ site/.vuepress/config.js
@@ -1,7 +1,6 @@
 module.exports = {
   title: "Frictionless Data",
   description: "Bringing simplicity and gracefulness to the data experience",
-  base: "/website-v2/",
   markdown: {
     linkify: true,
     typographer: true,
