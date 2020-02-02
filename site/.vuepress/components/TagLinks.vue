e69c3beb9aa74f8b2c191ead58cf2c1aca53ad9f
monikappv <monika.popova@datopian.com>
2020-02-03 00:12:35 +0200

    [blog][s]: Add tags

---
+++ site/.vuepress/components/TagLinks.vue
@@ -0,0 +1,11 @@
+<template lang="html">
+  <div>
+    <img class="w-6 h-6 inline text-lg" src="https://www.pngkey.com/png/full/349-3495832_tag-icon-png-file-tag-icon-grey.png"/>
+    <router-link
+      v-for="tag in $page.frontmatter.tags"
+      :key="tag"
+      :to="{ path: `/tags.html#${tag}`}">
+      {{tag}}
+    </router-link>
+  </div>
+</template>
