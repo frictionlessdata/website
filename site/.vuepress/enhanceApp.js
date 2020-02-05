3b968e0ed0488b968830fb38eacec5e7dd1d5bf2
ismail shahzad <ismail.shahzad@datopian.com>
2020-02-05 17:06:21 +0500

    [site/.vuepress/enhanceApp.js] Refactored the redirectList

--- site/.vuepress/enhanceApp.js
+++ site/.vuepress/enhanceApp.js
@@ -1,160 +1,294 @@
-const oldUrl= [
-    '/software/',
-    '/articles/',
-    '/docs/',
-    '/articles/nimblelearn-dpc/',
-    '/articles/datacurator/',
-    '/articles/nimblelearn/',
-    '/articles/center-for-data-science-and-public-policy-workforce-data-initiative/',
-    '/articles/openml/',
-    '/articles/zegami/',
-    '/articles/cmso/',
-    '/articles/collections-as-data/',
-    '/articles/the-data-retriever/',
-    '/articles/dataship/',
-    '/articles/dataworld/',
-    '/articles/john-snow-labs/',
-    '/articles/tesera/',
-    '/articles/open-power-system-data/',
-    '/articles/oleg-lavrovsky/',
-    '/articles/matt-thompson/',
-    '/articles/georges-labreche/',
-    '/articles/open-knowledge-greece/',
-    '/articles/ori-hoch/',
-    '/articles/daniel-fireman/',
-    '/articles/nes/',
-    '/articles/nes-tool/',
-    '/articles/andre-heughebaert/',
-    '/articles/stephan-max/',
-    '/articles/open-referral/',
-    '/articles/frictionless-darwincore/',
-    '/articles/open-referral-tool/',
-    '/articles/dm4t/',
-    '/articles/ukds/',
-    '/articles/university-of-pittsburgh/',
-    '/articles/elife/',
-    '/articles/causa-natura-pescando-datos/',
-    '/articles/university-of-cambridge/',
-    '/articles/pacific-northwest-national-laboratory-active-data-biology/',
-    '/docs/using-data-packages-in-clojure/',
-    '/docs/using-data-packages-in-java/',
-    '/docs/using-data-packages-in-go/',
-    '/docs/creating-tabular-data-packages-in-r/',
-    '/docs/using-data-packages-in-r/',
-    '/docs/using-data-packages-in-python/',
-    '/docs/creating-tabular-data-packages-in-python/',
-    '/docs/creating-tabular-data-packages-in-javascript/',
-    '/docs/joining-data-in-python/',
-    '/docs/joining-tabular-data-in-python/',
-    '/docs/publish-data-as-data-packages/',
-    '/docs/applying-licenses/',
-    '/docs/publish/',
-    '/docs/publish-online/',
-    '/docs/publish-any/',
-    '/docs/publish-tabular/',
-    '/docs/publish-geo/',
-    '/docs/publish-faq/',
-    '/docs/point-location-data/',
-    '/docs/csv/',
-    '/docs/validating-data/',
-    '/field-guide/well-packaged-datasets/',
-    '/field-guide/visible-findable-shareable-data/',
-    '/field-guide/validated-tabular-data/',
-    '/field-guide/used-and-useful-data/',
-    '/field-guide/automatically-validated-tabular-data/',
-    '/field-guide/data-publication-workflow-example/',
-    '/reproducible-research/',
-    '/docs/tutorial-template/',
-    '/docs/developer-guide/',
-    '/universe/',
-    '/docs/data-package/',
-    '/docs/table-schema/',
-    '/docs/tabular-data-package/',
-    '/field-guide/',
-    ];
+const redirectList= [
+  {
+    path: "/software/",
+    redirect: "/products/"
+  },
+  {
+    path: "/articles/",
+    redirect: "/blog/"
+  },
+  {
+    path: "/docs/",
+    redirect: "/blog/"
+  },
+  {
+    path: "/articles/nimblelearn-dpc/",
+    redirect: "/blog/2019-07-22/nimblelearn-dpc/"
+  },
+  {
+    path: "/articles/datacurator/",
+    redirect: "/blog/2019-03-01/datacurator/"  
+  },
+  {
+    path: "/articles/nimblelearn/",
+    redirect: "/blog/2018-07-20/nimblelearn/"
+  },
+  {
+    path: "/articles/center-for-data-science-and-public-policy-workforce-data-initiative/",
+    redirect: "/blog/2017-08-15/center-for-data-science-and-public-policy-workforce-data-initiative/"
+  },
+  {
+    path: "/articles/openml/",
+    redirect: "/blog/2017-12-04/openml/"
+  },
+  {
+    path: "/articles/zegami/",
+    redirect: "/blog/2017-09-28/zegami/"
+  },
+  {
+    path: "/articles/cmso/",
+    redirect: "/blog/2017-05-23/cmso/"
+  },
+  {
+    path: "/articles/collections-as-data/",
+    redirect: "/blog/2017-08-09/collections-as-data/"
+  },
+  {
+    path: "/articles/the-data-retriever/",
+    redirect: "/blog/2017-05-24/the-data-retriever/"
+  },
+  {
+    path: "/articles/dataship/",
+    redirect: "/blog/2016-11-15/dataship/"
+  },
+  {
+    path: "/articles/dataworld/",
+    redirect: "/blog/2017-04-11/dataworld/"
+  },
+  {
+    path: "/articles/john-snow-labs/",
+    redirect: "/blog/2017-03-28/john-snow-labs/"
+  },
+  {
+    path: "/articles/tesera/",
+    redirect: "/blog/2016-11-15/tesera/"
+  },
+  {
+    path: "/articles/open-power-system-data/",
+    redirect: "/blog/2016-11-15/open-power-system-data/"
+  },
+  {
+    path: "/articles/matt-thompson/",
+    redirect: "/blog/2017-10-26/matt-thompson/"
+  },
+  {
+    path: "/articles/georges-labreche/",
+    redirect: "/blog/2017-10-24/georges-labreche/"
+  },
+  {
+    path: "/articles/oleg-lavrovsky/",
+    redirect: "/blog/2018-07-16/oleg-lavrovsky/"
+  },
+  {
+    path: "/articles/open-knowledge-greece/",
+    redirect: "/blog/2017-10-27/open-knowledge-greece/"
+  },
+  {
+    path: "/articles/ori-hoch/",
+    redirect: "/blog/2018-07-16/ori-hoch/"
+  },
+  {
+    path: "/articles/daniel-fireman/",
+    redirect: "/blog/2017-11-01/daniel-fireman/"
+  },
+  {
+    path: "/articles/nes/",
+    redirect: "/blog/2019-07-03/nes/"
+  },
+  {
+    path: "/articles/nes-tool/",
+    redirect: "/blog/2020-01-23/nes-tool/"
+  },
+  {
+    path: "/articles/andre-heughebaert/",
+    redirect: "/blog/2019-09-12/andre-heughebaert/"
+  },
+  {
+    path: "/articles/stephan-max/",
+    redirect: "/blog/2019-07-02/stephan-max/"
+  },
+  {
+    path: "/articles/open-referral/",
+    redirect: "/blog/2019-07-09/open-referral/"
+  },
+  {
+    path: "/articles/frictionless-darwincore/",
+    redirect: "/blog/2020-01-22/frictionless-darwincore/"
+  },
+  {
+    path: "/articles/open-referral-tool/",
+    redirect: "/blog/2020-01-22/open-referral-tool/"
+  },
+  {
+    path: "/articles/dm4t/",
+    redirect: "/blog/2017-12-19/dm4t/"
+  },
+  {
+    path: "/articles/ukds/",
+    redirect: "/blog/2017-12-12/ukds/"
+  },
+  {
+    path: "/articles/university-of-pittsburgh/",
+    redirect: "/blog/2017-12-15/university-of-pittsburgh/"
+  },
+  {
+    path: "/articles/elife/",
+    redirect: "/blog/2017-10-24/elife/"
+  },
+  {
+    path: "/articles/causa-natura-pescando-datos/",
+    redirect: "/blog/2017-08-15/causa-natura-pescando-datos/"
+  },
+  {
+    path: "/articles/university-of-cambridge/",
+    redirect: "/blog/2017-08-15/university-of-cambridge/"
+  },
+  {
+    path: "/articles/pacific-northwest-national-laboratory-active-data-biology/",
+    redirect: "/blog/2018-05-07/pacific-northwest-national-laboratory-active-data-biology/"
+  },
+  {
+    path: "/docs/using-data-packages-in-clojure/",
+    redirect: "/blog/2018-05-07/using-data-packages-in-clojure/"
+  },
+  {
+    path: "/docs/using-data-packages-in-java/",
+    redirect: "/blog/2018-04-28/using-data-packages-in-java/"
+  },
+  {
+    path: "/docs/using-data-packages-in-go/",
+    redirect: "/blog/2018-02-16/using-data-packages-in-go/"
+  },
+  {
+    path: "/docs/creating-tabular-data-packages-in-r/",
+    redirect: "/blog/2018-02-14/creating-tabular-data-packages-in-r/"
+  },
+  {
+    path: "/docs/using-data-packages-in-r/",
+    redirect: "/blog/2018-02-14/using-data-packages-in-r/"
+  },
+  {
+    path: "/docs/using-data-packages-in-python/",
+    redirect: "/blog/2016-08-29/using-data-packages-in-python/"
+  },
+  {
+    path: "/docs/creating-tabular-data-packages-in-python/",
+    redirect: "/blog/2016-07-21/creating-tabular-data-packages-in-python/"
+  },
+  {
+    path: "/docs/creating-tabular-data-packages-in-javascript/",
+    redirect: "/blog/2018-04-04/creating-tabular-data-packages-in-javascript/"
+  },
+  {
+    path: "/docs/joining-data-in-python/",
+    redirect: "/blog/2018-04-06/joining-data-in-python/"
+  },
+  {
+    path: "/docs/joining-tabular-data-in-python/",
+    redirect: "/blog/2018-04-05/joining-tabular-data-in-python/"
+  },
+  {
+    path: "/docs/publish-data-as-data-packages/",
+    redirect: "/blog/2018-07-16/publish-data-as-data-packages/"
+  },
+  {
+    path: "/docs/applying-licenses/",
+    redirect: "/blog/2018-03-27/applying-licenses/"
+  },
+  {
+    path: "/docs/publish/",
+    redirect: "/blog/2016-08-30/publish/"
+  },
+  {
+    path: "/docs/publish-online/",
+    redirect: "/blog/2018-08-29/publish-online/"
     
-    
-const destinationUrl=[
-    '/products/',
-    '/blog/',
-    '/blog/',
-    '/blog/2019-07-22/nimblelearn-dpc/',
-    '/blog/2019-03-01/datacurator/',
-    '/blog/2018-07-20/nimblelearn/',
-    '/blog/2017-08-15/center-for-data-science-and-public-policy-workforce-data-initiative/',
-    '/blog/2017-12-04/openml/',
-    '/blog/2017-09-28/zegami/',
-    '/blog/2017-05-23/cmso/',
-    '/blog/2017-08-09/collections-as-data/',
-    '/blog/2017-05-24/the-data-retriever/',
-    '/blog/2016-11-15/dataship/',
-    '/blog/2017-04-11/dataworld/',
-    '/blog/2017-03-28/john-snow-labs/',
-    '/blog/2016-11-15/tesera/',
-    '/blog/2016-11-15/open-power-system-data/',
-    '/blog/2018-07-16/oleg-lavrovsky/',
-    '/blog/2017-10-26/matt-thompson/',
-    '/blog/2017-10-24/georges-labreche/',
-    '/blog/2017-10-27/open-knowledge-greece/',
-    '/blog/2018-07-16/ori-hoch/',
-    '/blog/2017-11-01/daniel-fireman/',
-    '/blog/2019-07-03/nes/',
-    '/blog/2020-01-23/nes-tool/',
-    '/blog/2019-09-12/andre-heughebaert/',
-    '/blog/2019-07-02/stephan-max/',
-    '/blog/2019-07-09/open-referral/',
-    '/blog/2020-01-22/frictionless-darwincore/',
-    '/blog/2020-01-22/open-referral-tool/',
-    '/blog/2017-12-19/dm4t/',
-    '/blog/2017-12-12/ukds/',
-    '/blog/2017-12-15/university-of-pittsburgh/',
-    '/blog/2017-10-24/elife/',
-    '/blog/2017-08-15/causa-natura-pescando-datos/',
-    '/blog/2017-08-15/university-of-cambridge/',
-    '/blog/2018-05-07/pacific-northwest-national-laboratory-active-data-biology/',
-    '/blog/2018-05-07/using-data-packages-in-clojure/',
-    '/blog/2018-04-28/using-data-packages-in-java/',
-    '/blog/2018-02-16/using-data-packages-in-go/',
-    '/blog/2018-02-14/creating-tabular-data-packages-in-r/',
-    '/blog/2018-02-14/using-data-packages-in-r/',
-    '/blog/2016-08-29/using-data-packages-in-python/',
-    '/blog/2016-07-21/creating-tabular-data-packages-in-python/',
-    '/blog/2018-04-04/creating-tabular-data-packages-in-javascript/',
-    '/blog/2018-04-06/joining-data-in-python/',
-    '/blog/2018-04-05/joining-tabular-data-in-python/',
-    '/blog/2018-07-16/publish-data-as-data-packages/',
-    '/blog/2018-03-27/applying-licenses/',
-    '/blog/2016-08-30/publish/',
-    '/blog/2018-08-29/publish-online/',
-    '/blog/2016-07-21/publish-any/',
-    '/blog/2016-07-21/publish-tabular/',
-    '/blog/2016-04-30/publish-geo/',
-    '/blog/2018-07-16/publish-faq/',
-    '/blog/2018-07-16/point-location-data/',
-    '/blog/2018-07-09/csv/',
-    '/blog/2018-07-09/validating-data/',
-    '/blog/2018-03-07/well-packaged-datasets/',
-    '/blog/2018-07-16/visible-findable-shareable-data/',
-    '/blog/2018-07-16/validated-tabular-data/',
-    '/blog/2019-05-20/used-and-useful-data/',
-    '/blog/2018-03-12/automatically-validated-tabular-data/',
-    '/blog/2018-03-12/data-publication-workflow-example/',
-    '/reproducible-research/',
-    '/contribute',
-    '/contribute',
-    '/data-packages/',
-    '/data-packages/',
-    '/table-schema/',
-    '/data-packages/',
-    '/data-packages/guide/',
-    ];
-
-let redirectList=[]
-
-for (let i = 0; i < ((oldUrl.length)); i++) {
-    redirectList[i] = {path: oldUrl[i], redirect: destinationUrl[i]}
-}
-console.log(redirectList)
+  },
+  {
+    path: "/docs/publish-any/",
+    redirect: "/blog/2016-07-21/publish-any/"
+  },
+  {
+    path: "/docs/publish-tabular/",
+    redirect: "/blog/2016-07-21/publish-tabular/"
+  },
+  {
+    path: "/docs/publish-geo/",
+    redirect: "/blog/2016-04-30/publish-geo/"
+  },
+  {
+    path: "/docs/publish-faq/",
+    redirect: "/blog/2018-07-16/publish-faq/"
+  },
+  {
+    path: "/docs/point-location-data/",
+    redirect: "/blog/2018-07-16/point-location-data/"
+  },
+  {
+    path: "/docs/csv/",
+    redirect: "/blog/2018-07-09/csv/"
+  },
+  {
+    path: "/docs/validating-data/",
+    redirect: "/blog/2018-07-09/validating-data/"
+  },
+  {
+    path: "/field-guide/well-packaged-datasets/",
+    redirect: "/blog/2018-03-07/well-packaged-datasets/"
+  },
+  {
+    path: "/field-guide/visible-findable-shareable-data/",
+    redirect: "/blog/2018-07-16/visible-findable-shareable-data/"
+  },
+  {
+    path: "/field-guide/validated-tabular-data/",
+    redirect: "/blog/2018-07-16/validated-tabular-data/"
+  },
+  {
+    path: "/field-guide/used-and-useful-data/",
+    redirect: "/blog/2019-05-20/used-and-useful-data/"
+  },
+  {
+    path: "/field-guide/automatically-validated-tabular-data/",
+    redirect: "/blog/2018-03-12/automatically-validated-tabular-data/"
+  },
+  {
+    path: "/field-guide/data-publication-workflow-example/",
+    redirect: "/blog/2018-03-12/data-publication-workflow-example/"
+  },
+  {
+    path: "/reproducible-research/",
+    redirect: "/reproducible-research/"
+  },
+  {
+    path: "/docs/tutorial-template/",
+    redirect: "/contribute/"
+  },
+  {
+    path: "/docs/developer-guide/",
+    redirect: "/contribute/"
+  },
+  {
+    path: "/universe/",
+    redirect: "/data-packages/"    
+  },
+  {
+    path: "/docs/data-package/",
+    redirect: "/data-packages/"
+  },
+  {
+    path: "/docs/table-schema/",
+    redirect: "/table-schema/" 
+  },
+  {
+    path: "/docs/tabular-data-package/",
+    redirect: "/data-packages/"    
+  },
+  {
+    path: "/field-guide/",
+    redirect: "/data-packages/guide/"
+  }   
+];
 
 export default ({ router }) => {
     router.addRoutes(redirectList)
