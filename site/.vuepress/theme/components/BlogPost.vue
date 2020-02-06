8c7ead586379fbb77bfec8641e0c9da2e1ad8a74
Bot <tuttle-bot@lexman.org>
2020-02-06 10:56:55 +0200

    [blogpost][xs]: Add go back button to the blog individual page

--- site/.vuepress/theme/components/BlogPost.vue
+++ site/.vuepress/theme/components/BlogPost.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="theme-container">
     <div class="container w-2/5 pt-56 mx-auto">
-    <h1 class="text-3xl pb-4  ">{{ $page.frontmatter.title }}</h1>
+    <a href="" class="font-light"> < Go back</a>
+    <h1 class="text-3xl pt-6 pb-4  ">{{ $page.frontmatter.title }}</h1>
     <p class="text-xl pb-12">{{ $page.frontmatter.summary }} </p>
     <time class="italic"> {{ formatDate($page.frontmatter.date) }} by Max Examplemann </time>
     <TagLinks class="pt-2"/>
@@ -16,7 +17,6 @@ import moment from 'moment'
 
 export default {
         methods: {
-            // Control how our data is formatted. You can change to format accoring to JS date format.
             formatDate(date, format = 'MMM D, YYYY') {
                 return moment(date).format(format)
             }
