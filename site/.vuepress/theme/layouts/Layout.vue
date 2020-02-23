9671049de216b03a8167872df760a44db06c76bc
Rufus Pollock <rufus.pollock@datopian.com>
2020-02-23 20:44:08 +0800

    [home][l]: major rework of content for home page - and in the process revert home page to default vuepress latyout (for now).
    * content: new tagline, description, what's special/different etc (redid almost all of the content)
      * moved to a more "vue" like setup with a clean simple tagline and framework language
    * layout: reverted to vuepress home page as this was easiest way to get content in for now and without worrying too much about the look (also found quite a decent default layout)
    * main layout: moved footer out of home page layout to main layout as that is where it should be
      * TODO: currently a bit broken both because of width and b/c background color got lost (should be an easy fix)

--- site/.vuepress/theme/layouts/Layout.vue
+++ site/.vuepress/theme/layouts/Layout.vue
@@ -48,6 +48,52 @@
         slot="bottom"
       />
     </Page>
+
+<footer class="banner fd-footer py-16"> 
+  <div class="banner flex flex-row">
+    <div class="w-1/3 text-center pl-10">
+      <img class="w-64" src="/img/home/logo-white-thicker.svg"></img>
+    </div>
+    <div class="container w-2/3 flex justify-center flex-row px-10">
+      <div class="w-1/4 text-left pr-20">
+        <h2 class="text-xl font-normal text-white">Supported by</h2>
+        <div><img class="w-32 mx-auto py-8" src="/img/home/alfred.svg"></img></div>
+        <div><img class="w-32 mx-auto" src="/img/home/open-data-institute.svg"></img></div>
+      </div>
+      <div class="w-1/4 text-left pr-20">
+        <h2 class="text-xl text-white font-normal">About</h2>
+        <div class="pt-6"><a href="/about/" class="text-white font-light text-lg">About</a></div>
+        <div class="pt-6"><a href="/team/" class="text-white font-light text-lg">Contact</a></div>
+        <div class="pt-6"><a href="https://okfn.org/privacy-policy/" class="text-white font-light text-lg">Privacy Policy</a></div>
+        <div class="pt-6"><a href="https://okfn.org/terms-of-use/" class="text-white font-light text-lg">Terms of Use</a></div>
+      </div>
+      <div class="w-1/4 text-left pr-20">
+        <h2 class="text-xl text-white font-normal">Help</h2>
+        <div class="pt-6"><a href="/contribute/" class="text-white font-light text-lg">Support</a></div>
+        <div class="pt-6"><a href="/" class="text-white font-light text-lg">Get started</a></div>
+        <div class="pt-6"><a href="/team/" class="text-white font-light text-lg">Community</a></div>
+        <!-- <div class="pt-6"><a href="/documentation/" class="text-white font-light text-lg">Documentation</a></div> -->
+      </div>
+      <div class="w-1/4 text-left pr-20">
+        <h2 class="text-xl text-white font-normal">Social</h2>
+        <div class="pt-4">
+          <img class="w-8 inline-block align-middle" src="/img/home/github.svg"></img>
+          <a href="https://github.com/frictionlessdata/" class="text-white inline pl-2 font-light text-lg">GitHub</a>
+        </div>  
+        <div class="pt-6">
+          <img class="w-8 inline-block align-middle" src="/img/home/twitter.svg"></img>
+          <a href="https://twitter.com/hashtag/frictionlessdata" class="text-white pl-2 inline font-light text-lg">Twitter</a>
+        </div>
+        <div class="pt-6 pr-10">
+          <img class="w-8 inline-block align-middle" src="/img/home/gitter.svg"></img>
+          <a href="https://gitter.im/frictionlessdata/chat" class="text-white pl-2 inline font-light text-lg">Gitter</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+
+
   </div>
 </template>
 
