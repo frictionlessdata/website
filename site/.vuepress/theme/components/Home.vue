2647342e5157c52f790efadc4ea93f5c22e4a139
roll <eskarev@gmail.com>
2021-05-25 15:19:03 +0200

    Returned the toolbox

--- site/.vuepress/theme/components/Home.vue
+++ site/.vuepress/theme/components/Home.vue
@@ -1,15 +1,11 @@
 <template>
   <div class="banner pt-8">
     <main class="main-section hero-home">
-      <div class="content flex flex-wrap px-24">
-        <div class="black-text flex-1 pt-4">
+      <div class="content flex flex-wrap items-center px-24" style="padding-top:2rem">
+        <div class="tagline black-text flex-1">
           <h3 class="text-4xl font-thin">Data software and standards</h3>
-          <p class="text-xl w-4/5 font-thin w-auto">{{ data.tagline || $description || 'Welcome to your VuePress site' }}</p>
-        </div>
-        <div class="box flex-1 mt-8">
-        <img src="/img/frictionless-color-full-logo.svg" height="175"/>
-          <!--note: fix styling in next div-->
-          <div class="flex flex-row flex-wrap" style="padding-top: 2rem;">
+          <p class="text-xl md:w-4/5 font-thin w-auto">{{ data.tagline || $description || 'Welcome to your VuePress site' }}</p>
+          <div class="flex flex-row flex-wrap">
           <a
             class="bg-transparent hero-button text-black hover:border-black outline-none cursor-pointer border border-solid border-black font-medium text-sm py-4 px-6 hover:border-transparent rounded-full"
             href="#video-section"
@@ -20,7 +16,11 @@
           >Get Started</a>
           </div>
         </div>
+        <div class="box flex-1 pl-10">
+          <img class="w-11/12 pt-10" v-if="data.heroImage" :src="$withBase(data.heroImage)" :alt="data.heroAlt || 'toolbox'" />
+        </div>
       </div>
+
       <section class="bg-secondary py-8" v-if="data.features && data.features.length">
         <div class="inner-container features text-black flex flex-row flex-wrap items-center content-center">
           <div class="feature md:flex-1 px-12" v-for="(feature, index) in data.features" :key="index">
