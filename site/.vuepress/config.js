b109648d6c24c0128a0ab7616ff66acbc69d2be4
Andres Vazquez <andres@data99.com.ar>
2022-03-31 06:31:19 -0300

    Remove Google Analytics and add Plausible (#297)
    * Remove Google Analytics and add Plausible
    
    * Simpler plausible

--- site/.vuepress/config.js
+++ site/.vuepress/config.js
@@ -146,12 +146,6 @@ module.exports = {
   },
   plugins: [
     [
-      "@vuepress/plugin-google-analytics",
-      {
-        ga: "UA-33874954-38",
-      },
-    ],
-    [
       "@vuepress/blog",
       {
         directories: [
@@ -213,6 +207,7 @@ module.exports = {
     ["@vuepress/back-to-top"],
   ],
   head: [
+    ["script", { src: "https://plausible.io/js/script.js", "data-domain": "frictionlessdata.io"}],
     ["script", { src: "https://unpkg.com/honeycomb-grid@3.1.3" }],
     ["script", { src: "https://unpkg.com/svg.js@2.7.1" }],
   ],
